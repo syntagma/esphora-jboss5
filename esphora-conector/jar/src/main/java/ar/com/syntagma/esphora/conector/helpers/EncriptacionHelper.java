7a22e9a4d54f583509f72d667392b73bf83d11cd
Sebastian Bromberg <seba@wagaduu.com>
2015-02-03 11:53:56 -0300

    Read p12 file working!

--- esphora-conector/jar/src/main/java/ar/com/syntagma/esphora/conector/helpers/EncriptacionHelper.java
+++ esphora-conector/jar/src/main/java/ar/com/syntagma/esphora/conector/helpers/EncriptacionHelper.java
@@ -1,7 +1,9 @@
 package ar.com.syntagma.esphora.conector.helpers;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
@@ -35,224 +37,235 @@ import com.sun.org.apache.xerces.internal.jaxp.datatype.XMLGregorianCalendarImpl
 @SuppressWarnings("restriction")
 @Name("encriptacionHelper")
 public class EncriptacionHelper {
-	//@Logger
-	//private static Log log;
+    //@Logger
+    //private static Log log;
 
-	//
-	// Create the CMS Message
-	//
-	
-	private Date fechaVencimientoCertificado;
-	
-	public byte[] crearCms(String p12file, String p12pass,
-			String signer, String dstDN, String service, Long TicketTime) throws ConectorException{
+    //
+    // Create the CMS Message
+    //
 
-		PrivateKey pKey = null;
-		X509Certificate pCertificate = null;
-		byte[] asn1Cms = null;
-		CertStore cstore = null;
-		String LoginTicketRequestXml = null;
-		String SignerDN = null;
+    private Date fechaVencimientoCertificado;
 
-		//
-		// Manage Keys & Certificates Base64.encode (loginTicketRequestXmlCms)
-		//
-			KeyStore ks = null;
-			try {
-				ks = KeyStore.getInstance("pkcs12");
-			} catch (KeyStoreException e4) {
-				throw new ConectorException(99999,
-						"Excepcion al instanciar el certificado", e4);
-			}
-			
-			InputStream p12stream = ResourceLoader.instance().getResourceAsStream("/"+p12file); //InputStream.class.getResourceAsStream(p12file); 
-				//Thread.currentThread().getContextClassLoader().getResourceAsStream(p12file);
-			System.out.println("Certificado Leido!: "+p12file);
-			
-			try {
-				ks.load(p12stream, p12pass.toCharArray());
-				
-				
-				
-				System.out.println("Certificado Cargado! " + ks.getProvider());
-			} catch (NoSuchAlgorithmException e) {
-				throw new ConectorException(99999,
-						"No se puede interpretar el algoritmo de encriptacion", e);
-			} catch (CertificateException e) {
-				throw new ConectorException(99999,
-						"No se puede leer el certificado .'12", e);
-			} catch (IOException e) {
-				throw new ConectorException(99999,"Error de I/O al cargar el archivo de certificados",e);
-			}
-			try {
-				System.out.println("Certificado Cargado! " + ks.size() + "-"+ ks.getType()
-				 + "-" + ks.toString());
-			} catch (KeyStoreException e4) {
-				throw new ConectorException(99999,
-						"No se puede leer el certificado .p12", e4);
-			}
-			try {
-				System.out.println("Cerrando p12stream");
-				p12stream.close();
-			} catch (IOException e3) {
-				e3.printStackTrace();
-				throw new ConectorException(99999,"Error de I/O al cerrar el archivo de certificados",e3);
-			} catch (Exception e3) {
-				e3.printStackTrace();
-				throw new ConectorException(99999,"Error de I/O al cerrar el archivo de certificados",e3);
-			}
+    public byte[] crearCms(String p12file, String p12pass,
+                           String signer, String dstDN, String service, Long TicketTime) throws ConectorException {
 
-			// Get Certificate & Private key from KeyStore
-			try {
-				System.out.println("Obtengo clave privada " + signer + " " + p12pass);
-				pKey = (PrivateKey) ks.getKey(signer, p12pass.toCharArray());
-				
-			} catch (UnrecoverableKeyException e2) {
-				throw new ConectorException(99999,
-						"No se puede recuperar la clave del certificado .p12", e2);
-			} catch (KeyStoreException e2) {
-				throw new ConectorException(99999,
-						"No se puede leer el certificado .p12", e2);
-			} catch (NoSuchAlgorithmException e2) {
-				throw new ConectorException(99999,
-						"No se encuentra el algoritmo", e2);
-			}
-			try {
-				System.out.println("Obtengo certificado");
-				pCertificate = (X509Certificate) ks.getCertificate(signer);
-				fechaVencimientoCertificado = pCertificate.getNotAfter();
-			} catch (KeyStoreException e2) {
-				e2.printStackTrace();
-			}
-			SignerDN = pCertificate.getSubjectDN().toString();
+        PrivateKey pKey = null;
+        X509Certificate pCertificate = null;
+        byte[] asn1Cms = null;
+        CertStore cstore = null;
+        String LoginTicketRequestXml = null;
+        String SignerDN = null;
 
-			// Create a list of Certificates to include in the final CMS
-			ArrayList<X509Certificate> certList = new ArrayList<X509Certificate>();
-			certList.add(pCertificate);
+        //
+        // Manage Keys & Certificates Base64.encode (loginTicketRequestXmlCms)
+        //
+        KeyStore ks = null;
+        try {
+            ks = KeyStore.getInstance("pkcs12");
+        } catch (KeyStoreException e4) {
+            throw new ConectorException(99999,
+                    "Excepcion al instanciar el certificado", e4);
+        }
 
-			if (Security.getProvider("BC") == null) {
-				Security.addProvider(new BouncyCastleProvider());
-			}
+        String path = System.getProperty("jboss.server.config.url") + p12file;
+        InputStream p12stream;
+        try {
+            p12stream = new URL(path).openStream(); //new FileInputStream(path));
+        } catch (IOException e) {
+            throw new ConectorException(99999, "No se ha podido leer el archivo " + path, e);
+        } catch (Exception e) {
+            throw new ConectorException(99999, "No se ha podido leer el archivo " + path, e);
+        }
 
-			try {
-				cstore = CertStore.getInstance("Collection",
-						new CollectionCertStoreParameters(certList), "BC");
-			} catch (InvalidAlgorithmParameterException e1) {
-				e1.printStackTrace();
-			} catch (NoSuchAlgorithmException e1) {
-				e1.printStackTrace();
-			} catch (NoSuchProviderException e1) {
-				e1.printStackTrace();
-			}
+        //InputStream p12stream = ResourceLoader.instance().getResourceAsStream(path); //InputStream.class.getResourceAsStream(p12file);
+        //Thread.currentThread().getContextClassLoader().getResourceAsStream(p12file);
+        System.out.println("Certificado Leido!: " + path);
+        System.out.println("P12 Stream!: " + p12stream.toString());
 
-		//
-		// Create XML Message
-		// 
-		LoginTicketRequestXml = crearLoginTicketRequest(SignerDN, dstDN,
-				service, TicketTime);
-		// log.info("XML: " + LoginTicketRequestXml);
-		//
-		// Create CMS Message
-		//
-			CMSSignedDataGenerator gen = new CMSSignedDataGenerator();
+        try {
+            ks.load(p12stream, p12pass.toCharArray());
 
-			// Add a Signer to the Message
-			gen.addSigner(pKey, pCertificate,
-					CMSSignedDataGenerator.DIGEST_SHA1);
-			// log.info("Key: " + pKey);
-			// log.info("Certificate: " + pCertificate);
 
-			
-			// Add the Certificate to the Message
-			try {
-				gen.addCertificatesAndCRLs(cstore);
-			} catch (CertStoreException e) {
-				e.printStackTrace();
-			} catch (CMSException e) {
-				e.printStackTrace();
-			}
+            System.out.println("Certificado Cargado! " + ks.getProvider());
+        } catch (NoSuchAlgorithmException e) {
+            throw new ConectorException(99999,
+                    "No se puede interpretar el algoritmo de encriptacion", e);
+        } catch (CertificateException e) {
+            throw new ConectorException(99999,
+                    "No se puede leer el certificado .'12", e);
+        } catch (IOException e) {
+            throw new ConectorException(99999, "Error de I/O al cargar el archivo de certificados", e);
+        }
+        try {
+            System.out.println("Certificado Cargado! " + ks.size() + "-" + ks.getType()
+                    + "-" + ks.toString());
+        } catch (KeyStoreException e4) {
+            throw new ConectorException(99999,
+                    "No se puede leer el certificado .p12", e4);
+        }
+        try {
+            System.out.println("Cerrando p12stream");
+            p12stream.close();
+        } catch (IOException e3) {
+            e3.printStackTrace();
+            throw new ConectorException(99999, "Error de I/O al cerrar el archivo de certificados", e3);
+        } catch (Exception e3) {
+            e3.printStackTrace();
+            throw new ConectorException(99999, "Error de I/O al cerrar el archivo de certificados", e3);
+        }
 
-			// Add the data (XML) to the Message
-			CMSProcessable data = new CMSProcessableByteArray(
-					LoginTicketRequestXml.getBytes());
+        // Get Certificate & Private key from KeyStore
+        try {
+            System.out.println("Obtengo clave privada " + signer + " " + p12pass);
+            pKey = (PrivateKey) ks.getKey(signer, p12pass.toCharArray());
 
-			// Add a Sign of the Data to the Message
-			CMSSignedData signed = null;
-			try {
-				signed = gen.generate(data, true, "BC");
-			} catch (NoSuchAlgorithmException e) {
-				e.printStackTrace();
-			} catch (NoSuchProviderException e) {
-				e.printStackTrace();
-			} catch (CMSException e) {
-				e.printStackTrace();
-			}
+        } catch (UnrecoverableKeyException e2) {
+            throw new ConectorException(99999,
+                    "No se puede recuperar la clave del certificado .p12", e2);
+        } catch (KeyStoreException e2) {
+            throw new ConectorException(99999,
+                    "No se puede leer el certificado .p12", e2);
+        } catch (NoSuchAlgorithmException e2) {
+            throw new ConectorException(99999,
+                    "No se encuentra el algoritmo", e2);
+        }
+        try {
+            System.out.println("Obtengo certificado X509");
+            pCertificate = (X509Certificate) ks.getCertificate(signer);
+            System.out.println("Certificado X509 Obtenido: " + pCertificate.getType() );
+            fechaVencimientoCertificado = pCertificate.getNotAfter();
+        } catch (KeyStoreException e2) {
+            e2.printStackTrace();
+        }
+        SignerDN = pCertificate.getSubjectDN().toString();
 
-			// 
-			try {
-				asn1Cms = signed.getEncoded();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			// log.info("signed: " + signed.getSignerInfos().getSigners() );
-			// log.info("asn: " +asn1Cms.toString());
+        // Create a list of Certificates to include in the final CMS
+        ArrayList<X509Certificate> certList = new ArrayList<X509Certificate>();
+        certList.add(pCertificate);
 
-		return (asn1Cms);
-	}
+        if (Security.getProvider("BC") == null) {
+            Security.addProvider(new BouncyCastleProvider());
+        }
 
-	//
-	// Create XML Message for AFIP wsaa
-	// 	
-	public static String crearLoginTicketRequest(String SignerDN, String dstDN,
-			String service, Long TicketTime) {
+        try {
+            cstore = CertStore.getInstance("Collection",
+                    new CollectionCertStoreParameters(certList), "BC");
+        } catch (InvalidAlgorithmParameterException e1) {
+            e1.printStackTrace();
+        } catch (NoSuchAlgorithmException e1) {
+            e1.printStackTrace();
+        } catch (NoSuchProviderException e1) {
+            e1.printStackTrace();
+        }
 
-		String loginTicketRequestXml;
+        //
+        // Create XML Message
+        //
+        LoginTicketRequestXml = crearLoginTicketRequest(SignerDN, dstDN,
+                service, TicketTime);
+        // log.info("XML: " + LoginTicketRequestXml);
+        //
+        // Create CMS Message
+        //
+        CMSSignedDataGenerator gen = new CMSSignedDataGenerator();
 
-		Date GenTime = new Date();
-		GregorianCalendar gentime = new GregorianCalendar();
-		GregorianCalendar exptime = new GregorianCalendar();
-		String UniqueId = new Long(GenTime.getTime() / 1000).toString();
+        // Add a Signer to the Message
+        gen.addSigner(pKey, pCertificate,
+                CMSSignedDataGenerator.DIGEST_SHA1);
+        // log.info("Key: " + pKey);
+        // log.info("Certificate: " + pCertificate);
 
-		exptime.setTime(new Date(GenTime.getTime() + TicketTime));
 
-		XMLGregorianCalendarImpl XMLGenTime = new XMLGregorianCalendarImpl(
-				gentime);
-		XMLGregorianCalendarImpl XMLExpTime = new XMLGregorianCalendarImpl(
-				exptime);
-		loginTicketRequestXml = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>"
-				+ "<loginTicketRequest version=\"1.0\">"
-				+ "<header>"
-				+ "<source>"
-				+ SignerDN
-				+ "</source>"
-				+ "<destination>"
-				+ dstDN
-				+ "</destination>"
-				+ "<uniqueId>"
-				+ UniqueId
-				+ "</uniqueId>"
-				+ "<generationTime>"
-				+ XMLGenTime
-				+ "</generationTime>"
-				+ "<expirationTime>"
-				+ XMLExpTime
-				+ "</expirationTime>"
-				+ "</header>"
-				+ "<service>"
-				+ service
-				+ "</service>"
-				+ "</loginTicketRequest>";
+        // Add the Certificate to the Message
+        try {
+            gen.addCertificatesAndCRLs(cstore);
+        } catch (CertStoreException e) {
+            e.printStackTrace();
+        } catch (CMSException e) {
+            e.printStackTrace();
+        }
 
-		return (loginTicketRequestXml);
-	}
+        // Add the data (XML) to the Message
+        CMSProcessable data = new CMSProcessableByteArray(
+                LoginTicketRequestXml.getBytes());
 
-	public Date getFechaVencimientoCertificado() {
-		return fechaVencimientoCertificado;
-	}
+        // Add a Sign of the Data to the Message
+        CMSSignedData signed = null;
+        try {
+            signed = gen.generate(data, true, "BC");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+        } catch (CMSException e) {
+            e.printStackTrace();
+        }
 
-	public void setFechaVencimientoCertificado(
-			Date fechaVencimientoCertificado) {
-		this.fechaVencimientoCertificado = fechaVencimientoCertificado;
-	}
+        //
+        try {
+            asn1Cms = signed.getEncoded();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // log.info("signed: " + signed.getSignerInfos().getSigners() );
+        // log.info("asn: " +asn1Cms.toString());
+
+        return (asn1Cms);
+    }
+
+    //
+    // Create XML Message for AFIP wsaa
+    //
+    public static String crearLoginTicketRequest(String SignerDN, String dstDN,
+                                                 String service, Long TicketTime) {
+
+        String loginTicketRequestXml;
+
+        Date GenTime = new Date();
+        GregorianCalendar gentime = new GregorianCalendar();
+        GregorianCalendar exptime = new GregorianCalendar();
+        String UniqueId = new Long(GenTime.getTime() / 1000).toString();
+
+        exptime.setTime(new Date(GenTime.getTime() + TicketTime));
+
+        XMLGregorianCalendarImpl XMLGenTime = new XMLGregorianCalendarImpl(
+                gentime);
+        XMLGregorianCalendarImpl XMLExpTime = new XMLGregorianCalendarImpl(
+                exptime);
+        loginTicketRequestXml = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>"
+                + "<loginTicketRequest version=\"1.0\">"
+                + "<header>"
+                + "<source>"
+                + SignerDN
+                + "</source>"
+                + "<destination>"
+                + dstDN
+                + "</destination>"
+                + "<uniqueId>"
+                + UniqueId
+                + "</uniqueId>"
+                + "<generationTime>"
+                + XMLGenTime
+                + "</generationTime>"
+                + "<expirationTime>"
+                + XMLExpTime
+                + "</expirationTime>"
+                + "</header>"
+                + "<service>"
+                + service
+                + "</service>"
+                + "</loginTicketRequest>";
+
+        return (loginTicketRequestXml);
+    }
+
+    public Date getFechaVencimientoCertificado() {
+        return fechaVencimientoCertificado;
+    }
+
+    public void setFechaVencimientoCertificado(
+            Date fechaVencimientoCertificado) {
+        this.fechaVencimientoCertificado = fechaVencimientoCertificado;
+    }
 
 }
