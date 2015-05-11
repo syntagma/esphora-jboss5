2f97a72501eec2b80d5a9ca943d6cb8687a5811a
Sebastian Bromberg <seba@wagaduu.com>
2015-05-11 16:18:51 -0300

    New proporties from CONF

--- esphora-conector-gui/jar/src/main/java/ar/com/syntagma/esphora/conector/factories/PropiedadesFactory.java
+++ esphora-conector-gui/jar/src/main/java/ar/com/syntagma/esphora/conector/factories/PropiedadesFactory.java
@@ -1,57 +1,50 @@
 package ar.com.syntagma.esphora.conector.factories;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.net.URL;
 import java.util.Properties;
 
 public class PropiedadesFactory {
 
-	public static Properties getArchivoAmbiente() {
-		InputStream is = null;
-		Properties propiedades = new Properties();
+    public static Properties getArchivoAmbiente() {
+        Properties propiedades = new Properties();
 
-		is = Thread.currentThread().getContextClassLoader()
-				.getResourceAsStream("/ambiente.properties");
-		try {
-			propiedades.load(is);
-		} catch (IOException e) {
-			throw new RuntimeException(
-					"No se ha podido leer el archivo ambiente.properties", e);
-		} catch (Exception e) {
-			throw new RuntimeException(
-					"No se ha podido leer el archivo ambiente.properties", e);
-		}
-		return propiedades;
-	}
+        String path = System.getProperty("jboss.server.config.url") + "ambiente.properties";
+        try {
+            propiedades.load(new URL(path).openStream()); //new FileInputStream(path));
+        } catch (IOException e) {
+            throw new RuntimeException("No se ha podido leer el archivo ambiente.properties", e);
+        } catch (Exception e) {
+            throw new RuntimeException("No se ha podido leer el archivo ambiente.properties", e);
+        }
+        return propiedades;
+    }
 
-	public static Properties getConectorProperties() {
-		InputStream is = null;
-		String path = "";
-		Properties propiedades = new Properties();
+    public static Properties getConectorProperties() {
+        Properties propiedades = new Properties();
 
-		String a = PropiedadesFactory.getArchivoAmbiente().getProperty(
-				"ambiente");
+        String path = System.getProperty("jboss.server.config.url") + "ambiente.properties";
 
-		if (a.equals("test")) {
-			path = "test/";
-		}
-		if (a.equals("produccion")) {
-			path = "prod/";
-		}
+        String a = PropiedadesFactory.getArchivoAmbiente().getProperty(
+                "ambiente");
 
-		String fileName = path + "conector.properties";
-		is = Thread.currentThread().getContextClassLoader()
-				.getResourceAsStream(fileName);
-		try {
-			propiedades.load(is);
-		} catch (IOException e) {
-			throw new RuntimeException("No se ha podido leer el archivo "
-					+ fileName, e);
-		} catch (Exception e) {
-			throw new RuntimeException("No se ha podido leer el archivo "
-					+ fileName, e);
-		}
-		return propiedades;
-	}
+        if (a.equals("test")) {
+            path = path + "test/";
+        }
+        if (a.equals("produccion")) {
+            path = path + "prod/";
+        }
 
+        String fileName = path + "conector.properties";
+
+        try {
+            propiedades.load(new URL(fileName).openStream()); //new FileInputStream(path));
+        } catch (IOException e) {
+            throw new RuntimeException("No se ha podido leer el archivo ambiente.properties", e);
+        } catch (Exception e) {
+            throw new RuntimeException("No se ha podido leer el archivo ambiente.properties", e);
+        }
+        return propiedades;
+
+    }
 }
